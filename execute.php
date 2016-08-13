ab3ffa00dae003c549a8d622c69d0149f93c82ec
gabry9306 <gabrieledellaria34@gmail.com>
2016-08-14 01:07:59 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -122,7 +122,7 @@ elseif(strpos($text, "/ristoro") === 0 || $text == "\xF0\x9F\x8D\x94 PUNTI RISTO
 
 }
 
-switch($text) {
+switch($message) {
 	
 	case "\xF0\x9F\x8D\x94 PANINERIA JHONNY":
 		$response = "\xF0\x9F\x8D\x94 Panineria da Jhonny \n\n\xF0\x9F\x8C\x8E Localizzazione Maps \xF0\x9F\x8C\x8E\n\nhttps://goo.gl/a17ehV\n\n\xF0\x9F\x95\x92 Orari Esercizio\n\nLun-Ven dalle 11.30 alle 23.30\n\nDa Settembre a Luglio\n\n";
