a9d082c00476b78311dc1e946696fe9c4dc48649
gabry9306 <gabrieledellaria34@gmail.com>
2016-08-14 01:46:36 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -188,9 +188,9 @@ elseif(strpos($text, "/orariolezioni") === 0 || $text == "\xF0\x9F\x95\x92 ORARI
 	
 	$postField = array(
 		 'chat_id' => $chatId, 
-		 'text' => "\xE2\x9A\xA0 Scegli Facoltà \xE2\x9A\xA0\n\n/orariolezing Corsi Ingegneria \n\n/orariolezarch Corsi Architettura \n\n", 
+		 'text' => "\xF0\x9F\x95\x92 SEZIONE ORARIO LEZIONI \xF0\x9F\x95\x92\n\nScegli \n\n", 
 		 'reply_markup' => array(
-			 "keyboard"=> array(array("\xF0\x9F\x91\xA5 MENU STUDENTI","\xF0\x9F\x95\x92 ORARIO ING"),array("\xF0\x9F\x95\x92 ORARIO ARCH","\xF0\x9F\x95\x92 ORARIO ECO"))
+			 "keyboard"=> array(array("\xF0\x9F\x91\xA5 MENU STUDENTI","\xF0\x9F\x95\x92 ORARIO CORSI ING"),array("\xF0\x9F\x95\x92 ORARIO CORSI ARCH","\xF0\x9F\x95\x92 ORARIO CORSI ECO"))
 			,'resize_keyboard' => true
 		)
 	);
