ad4fa3191b0e21aabb940c2171c161b45705dcef
gabry9306 <gabrieledellaria34@gmail.com>
2016-09-16 16:55:35 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -3,7 +3,7 @@ define("BOT_TOKEN", "254111121:AAE898EquNqARS_8UpwVepo131EdLNXLm2o");
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
-include("aule.php");
+include 'aule.php';
 
 if(!$update)
 {
@@ -55,38 +55,6 @@ if(strpos($text, "/start") === 0 || $text=="\xF0\x9F\x94\xB4 START" || $text == 
 	
 	//$response = "\xF0\x9F\x91\x8B Ciao $firstname (@$username), benvenuto in Unipa Bot!";
 }
-
-// AULE
-else if(strpos($text, "/aule") === 0 || $text == "\xF0\x9F\x8C\x8E AULE" || $text == "\xF0\x9F\x8C\x8E aule")
-{
-	
-	$message = isset($update['message']) ? $update['message'] : "";
-	$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-	$text = isset($message['text']) ? $message['text'] : "";
-	$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
-	// change image name and path
-	$postFields = array('chat_id' => $chatId, 
-						'photo' => new CURLFile(realpath("./img/mappaunipa.jpg")), 
-						'caption' => "Mappa Unipa"/*$text*/);
-	$ch = curl_init(); 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
-	curl_setopt($ch, CURLOPT_URL, $botUrl); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-	// read curl response
-	$output = curl_exec($ch);
-
- //comandi standard telegram da docs:
- //sendPhoto
- //sendAudio
- //sendDocument
- //sendSticker
- //sendVideo
- //sendVoice
- //sendLocation
- //sendChatAction
- }
-
 
 else if(strpos($text, "/help") === 0 || $text == "\xE2\x81\x89 HELP" || $text == "\xE2\x81\x89 help")
 {
