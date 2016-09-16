3ced1e65354ed3ff1b397c51406b325018de5bd0
gabry9306 <gabrieledellaria34@gmail.com>
2016-09-16 16:53:31 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -55,6 +55,39 @@ if(strpos($text, "/start") === 0 || $text=="\xF0\x9F\x94\xB4 START" || $text == 
 	
 	//$response = "\xF0\x9F\x91\x8B Ciao $firstname (@$username), benvenuto in Unipa Bot!";
 }
+
+// AULE
+else if(strpos($text, "/aule") === 0 || $text == "\xF0\x9F\x8C\x8E AULE" || $text == "\xF0\x9F\x8C\x8E aule")
+{
+	
+	$message = isset($update['message']) ? $update['message'] : "";
+	$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+	$text = isset($message['text']) ? $message['text'] : "";
+	$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
+	// change image name and path
+	$postFields = array('chat_id' => $chatId, 
+						'photo' => new CURLFile(realpath("./img/mappaunipa.jpg")), 
+						'caption' => "Mappa Unipa"/*$text*/);
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+	curl_setopt($ch, CURLOPT_URL, $botUrl); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+	// read curl response
+	$output = curl_exec($ch);
+
+ //comandi standard telegram da docs:
+ //sendPhoto
+ //sendAudio
+ //sendDocument
+ //sendSticker
+ //sendVideo
+ //sendVoice
+ //sendLocation
+ //sendChatAction
+ }
+
+
 else if(strpos($text, "/help") === 0 || $text == "\xE2\x81\x89 HELP" || $text == "\xE2\x81\x89 help")
 {
 	$response = "\xF0\x9F\x93\x94 ELENCOCOMANDI\n\n/start \xF0\x9F\x9A\x80 START BOT \n\n/professori \xF0\x9F\x91\xA4 Professori \n\n/mappa \xF0\x9F\x8C\x90 Mappa Unipa \n\n/orariolezioni \xF0\x9F\x95\x92 Orario Lezioni \n\n/oraribiblioteca \xF0\x9F\x8F\xA6 Orari Biblioteca \n\n/ristoro \xF0\x9F\x8D\x9D Punti Ristoro \n\n/about \xE2\x9A\xA0 Info sul Bot \n\n/help \xE2\x84\xB9 Elenco comandi \n\n";
