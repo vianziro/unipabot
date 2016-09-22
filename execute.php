0cc8376c302ae2d7e33df31a4bacaa5daea90409
gabry9306 <gabrieledellaria34@gmail.com>
2016-09-22 19:42:11 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -2791,6 +2791,22 @@ elseif(strpos($text, "/mappaarch") === 0 || $text == "🌍 MAPPA ARCHITETTURA" |
 	$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
 	// change image name and path
 	$postFields = array('chat_id' => $chatId, 
+						'photo' => new CURLFile(realpath("./img/mappaarchp2.jpg")), 
+						'caption' => $firstname.", ecco la Mappa di Architettura!");
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+	curl_setopt($ch, CURLOPT_URL, $botUrl); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+	// read curl response
+	$output = curl_exec($ch);
+
+	$message = isset($update['message']) ? $update['message'] : "";
+	$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+	$text = isset($message['text']) ? $message['text'] : "";
+	$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
+	// change image name and path
+	$postFields = array('chat_id' => $chatId, 
 						'photo' => new CURLFile(realpath("./img/mappaarchp3.jpg")), 
 						'caption' => $firstname.", ecco la Mappa di Architettura!");
 	$ch = curl_init(); 
@@ -2808,22 +2824,6 @@ elseif(strpos($text, "/mappaarch") === 0 || $text == "🌍 MAPPA ARCHITETTURA" |
 	// change image name and path
 	$postFields = array('chat_id' => $chatId, 
 						'photo' => new CURLFile(realpath("./img/mappaarchp4.jpg")), 
-						'caption' => $firstname.", ecco la Mappa di Architettura!");
-	$ch = curl_init(); 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
-	curl_setopt($ch, CURLOPT_URL, $botUrl); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-	// read curl response
-	$output = curl_exec($ch);
-
-	$message = isset($update['message']) ? $update['message'] : "";
-	$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-	$text = isset($message['text']) ? $message['text'] : "";
-	$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
-	// change image name and path
-	$postFields = array('chat_id' => $chatId, 
-						'photo' => new CURLFile(realpath("./img/mappaarchp5.jpg")), 
 						'caption' => $firstname.", ecco la Mappa di Architettura!");
 	$ch = curl_init(); 
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
