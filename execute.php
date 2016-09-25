4967c6a9e43a48c3e8a6959a52ef21fe82e1cc06
gabry9306 <gabrieledellaria34@gmail.com>
2016-09-26 00:01:17 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -7620,8 +7620,8 @@ elseif(strpos($text, "F010") === 0 || $text == "f010")
 		$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendLocation";
 		// change file name and path
 		$postFields = array('chat_id' => $chatId, 
-						'latitude' => "38.10209", 
-						'longitude' => "13.3451011");
+						'latitude' => "38.1038976", 
+						'longitude' => "13.3431922");
 		$ch = curl_init(); 
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
 		curl_setopt($ch, CURLOPT_URL, $botUrl); 
