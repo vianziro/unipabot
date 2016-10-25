23e00804a69ab5591c1b23244917af41ae3c1dd5
gabry9306 <gabrieledellaria34@gmail.com>
2016-10-25 11:46:45 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -32,35 +32,27 @@ $text = strtolower($text);
 header("Content-Type: application/json");
 
 
-else if( isset($update['inline_query']) ) {
-  $query_id     = $update['inline_query']['id'];
-  $query_text   = $update['inline_query']['query'];
-  $chat_id      = $update['inline_query']['from']['id'];
+$message_inline = isset($update['inline_query']) ? $update['inline_query'] : "";
 
-  $method='answerInlineQuery';
+$message_inline_Id = isset($message_inline['id']) ? $message_inline['id'] : "";
 
-  $inlineQueryResults = array(
-    array(
-      'type' => 'article',
-      'id' => '0',
-      'title' => 'Article One',
-      'message_text' => 'This is an example for article one'
-    ),
-    array(
-      'type' => 'article',
-      'id' => '0',
-      'title' => 'Article Two',
-      'message_text' => 'This is an example for article two'
-    )
-  );
-  //$msg = serialize($inlineQueryResults);
-  //file_get_contents(API_URL."/sendMessage?chat_id=".$chat_id."&text=".urlencode($msg));
-  //file_get_contents(API_URL."/answerInlineQuery?inline_query_id=".$query_id."&results=".$msg);
-  
-  $postField_inline = array(
-	'inline_query_id' => $query_id,
+$method='answerInlineQuery';
+$botToken="240736726:AAHGVsRYjCUw8LZOcs7BD9L9c_vcVY1xBIs";
+
+$postField_inline = array(
+	'inline_query_id' => $message_inline_Id, 
 	'cache_time' => 1,
-	'results' => $inlineQueryResults
+	'results' => array(
+		 'type' => 'article'
+		,'id' => 'invito'.rand(0,65535)
+		,'title' => 'prova msg'
+		,'message_text' => 'msg'
+		,'description' => 'Prova description'
+		,'reply_markup'=>['inline_keyboard'=>[
+			[	 ['text'=>'testo pulsante','url'=>"http://robylandia.net" ] ]
+		]]
+
+		),
 );
 
 $handle=curl_init();
@@ -74,8 +66,6 @@ curl_setopt($handle,CURLOPT_ENCODING,1);
 $dati=json_decode( curl_exec($handle) ,true);	
 
 curl_close($handle);
- 
-}
 
 fwrite($fHandle,"\n\nRisposta ricevuta da telegram:\n$dati");
 
