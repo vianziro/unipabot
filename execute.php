422f0028dd21fcf0ca3c8377ae4096ff810005cf
gabry9306 <gabrieledellaria34@gmail.com>
2016-11-01 13:31:57 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -53,7 +53,7 @@ $postField_inline = array(
 		,'message_text' => '🌥 METEO'		
 		,'description' => 'Scopri il meteo di Palermo'		
 		,'reply_markup'=>['inline_keyboard'=>[
-			[	 ['text'=>'🌥 METEO','callback_data'=> "/start" ] ]
+			[	 ['text'=>'CACCA','callback_data'=> "CACCA" ] ]
 		]]
 	))
 );
