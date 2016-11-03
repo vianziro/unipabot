d9d70d10344e4252bbafc80c51aa60dad621db4d
gabry9306 <gabrieledellaria34@gmail.com>
2016-11-03 12:58:13 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -40,7 +40,7 @@ $message_inline = isset($update['inline_query']) ? $update['inline_query'] : "";
 
 $message_inline_keyboard = isset($message_inline['callback_query']) ? $message_inline['callback_query'] : "";
 
-$message_inline_callbackdata = isset($message_inline_keyboard['text']) ? $message_inline_keyboard['text'] : "";
+$message_inline_callbackdata = isset($message_inline_keyboard['message']) ? $message_inline_keyboard['message'] : "";
 
 
 $message_inline_Id = isset($message_inline['id']) ? $message_inline['id'] : "";
