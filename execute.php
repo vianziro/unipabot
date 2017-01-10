6e5c019324a7f171f400dfe1205ec38b37cf5ada
gabry9306 <gabrieledellaria34@gmail.com>
2017-01-10 18:13:39 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -14089,10 +14089,10 @@ elseif(strpos($text, "/player") === 0 )
 		$message = isset($update['message']) ? $update['message'] : "";
 		$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 		$text = isset($message['text']) ? $message['text'] : "";
-		$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendVoice";
+		$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendAudio";
 		// change file name and path
 		$postFields = array('chat_id' => $chatId, 
-						'voice' => "http://icecast.unitedradio.it/virgin.mp3");
+						'audio' => "http://icecast.unitedradio.it/virgin.mp3");
 		$ch = curl_init(); 
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
 		curl_setopt($ch, CURLOPT_URL, $botUrl); 
